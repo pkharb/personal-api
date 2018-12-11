dec5a9f35398d4a4f2e34af07c64e1b1e7393ef9
pk <pradeepkharb@hotmail.com>
2018-12-11 12:40:35 -0800

    added home page, styling

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ require('dotenv').config();
 const express = require('express'),
       app = express(),
       PORT = process.env.PORT || 3000,
-      routesInfo = require('./models/endpoint');
+      routesInfo = require('./models/endpoint'),
+      path = require('path');
 
 
 // database connection
 require('./db');
 
 // app middleware
+app.use(express.static(path.join(__dirname, 'public', 'views')));
 app.use(express.json());
 
 // routes
@@ -21,7 +23,9 @@ app.use('/api/profile', profileRouter);
 app.use('/api/info', (req, res) => {
     res.json({ success: true, routesInfo});
 });
-
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
 // listen on PORT
 app.listen(PORT, err => {
     console.log(err || `server listening to ${PORT}`);
